e2c268bc33bae98cc151877f1c1e0c74dbc35a5e
clayton <claytonmagalhaes@upf.br>
2026-06-02 01:15:58 -0300

    add breeze on project

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,45 +1,41 @@
-@extends('layouts.crud')
+<x-app-layout>
 
-@section('title', 'Cadastrar produto')
-@section('content')
+    <form class="w-full max-w-md mx-auto bg-white dark:bg-gray-800 p-6 rounded-lg shadow" action="{{ url('products/new') }}" method="POST">
+        @csrf
 
-<form class="w-full max-w-md bg-white dark:bg-gray-800 p-6 rounded-lg
-shadow" action="{{ url('products/new') }}" method="POST">
-@csrf
+        <h1 class="text-2xl font-bold mb-6 text-gray-900 dark:text-white">Cadastrar Produto</h1>
 
-    <h1 class="text-2xl font-bold mb-6 text-gray-900 dark:text-white">Cadastrar Produto</h1>
+        @if($errors->any())
+        <div>
+            @foreach($errors->all() as $error)
+            {{ $error }}
+            @endforeach
+        </div>
+        @endif
 
-    @if($errors->any())
-    <div>
-        @foreach($errors->all() as $error)
-        {{ $error }}
-        @endforeach
-    </div>
-    @endif
+        <label class="block mb-1 text-gray-700 dark:text-gray-300">Nome:</label>
+        <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" required name="name" type="text" />
+        <label class="block mb-1 text-gray-700 dark:text-gray-300">Descrição:</label>
+        <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="description" type="textarea" />
+        <label class="block mb-1 text-gray-700 dark:text-gray-300">Quantidade:</label>
+        <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="quantity" type="number" />
+        <label class="block mb-1 text-gray-700 dark:text-gray-300">Preço:</label>
+        <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="price" type="number" />
 
-    <label class="block mb-1 text-gray-700 dark:text-gray-300">Nome:</label>
-    <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" required name="name" type="text" />
-    <label class="block mb-1 text-gray-700 dark:text-gray-300">Descrição:</label>
-    <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="description" type="textarea" />
-    <label class="block mb-1 text-gray-700 dark:text-gray-300">Quantidade:</label>
-    <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="quantity" type="number" />
-    <label class="block mb-1 text-gray-700 dark:text-gray-300">Preço:</label>
-    <input class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="price" type="number" />
+        <label class="block mb-1 text-gray-700 dark:text-gray-300">Tipo do produto:</label>
 
-    <label class="block mb-1 text-gray-700 dark:text-gray-300">Tipo do produto:</label>
+        <select class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="type_id">
+            <option value="">Selecione</option>
 
-    <select class="w-full p-2 mb-4 rounded border dark:bg-gray-700 dark:text-white" name="type_id">
-        <option value="">Selecione</option>
+            @foreach($types as $type)
+            <option value="{{ $type->id }}">
+                {{ $type->name }}
+            </option>
+            @endforeach
 
-        @foreach($types as $type)
-        <option value="{{ $type->id }}">
-            {{ $type->name }}
-        </option>
-        @endforeach
+        </select>
 
-    </select>
+        <input class="w-full p-2 rounded bg-blue-600 text-white" type="submit" value="Salvar" />
+    </form>
 
-    <input class="w-full p-2 rounded bg-blue-600 text-white" type="submit" value="Salvar" />
-</form>
-
-@endsection
+</x-app-layout>
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,27 +1,49 @@
-<form action="{{ url('products/update') }}" method="POST">
-    @csrf
-    <!-- campo oculto passando o ID como parâmetro no request -->
-    <input type="hidden" name="id" value="{{ $product['id'] }}">
-    <label>Nome:</label><br>
-    <input name="name" type="text" value="{{ $product['name'] }}" /><br>
-    <label>Descrição:</label><br>
-    <input name="description" type="textarea" value="{{
-$product['description'] }}" /><br>
-    <label>Quantidade:</label><br>
-    <input name="quantity" type="number" value="{{ $product['quantity']
-}}" /><br>
-    <label>Preço:</label><br>
-    <input name="price" type="number" value="{{ $product['price'] }}" /><br>
-    <label>Tipo do produto:</label><br>
-    <select name="type_id">
-        <option value="">Selecione</option>
+<x-app-layout>
 
-        @foreach($types as $type)
-        <option value="{{ $type->id }}" @selected($product->type_id
-            == $type->id)>
-            {{ $type->name }}
-        </option>
-        @endforeach
-    </select><br><br>
-    <input type="submit" value="Salvar" />
-</form>
+    <form class="w-full max-w-md mx-auto bg-white dark:bg-gray-800 p-6 rounded-lg shadow" action="{{ url('products/update') }}" method="POST">
+        @csrf
+        <!-- campo oculto passando o ID como parâmetro no request -->
+        <input type="hidden" name="id" value="{{ $product['id'] }}">
+
+        <div>
+            <x-input-label for="name" :value="__('Nome')" />
+            <x-text-input id="name" name="name" type="text" class="block mt-1 w-full" :value="old('name', $product['name'])" required autofocus />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="description" :value="__('Descrição')" />
+            <x-text-input id="description" name="description" type="text" class="block mt-1 w-full" :value="old('description', $product['description'])" />
+            <x-input-error :messages="$errors->get('description')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="quantity" :value="__('Quantidade')" />
+            <x-text-input id="quantity" name="quantity" type="number" class="block mt-1 w-full" :value="old('quantity', $product['quantity'])" />
+            <x-input-error :messages="$errors->get('quantity')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="price" :value="__('Preço')" />
+            <x-text-input id="price" name="price" type="number" class="block mt-1 w-full" :value="old('price', $product['price'])" />
+            <x-input-error :messages="$errors->get('price')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="type_id" :value="__('Tipo do produto')" />
+            <select id="type_id" name="type_id" class="block mt-1 w-full rounded border dark:bg-gray-700 dark:text-white p-2">
+                <option value="">Selecione</option>
+                @foreach($types as $type)
+                <option value="{{ $type->id }}" @selected(old('type_id', $product->type_id) == $type->id)>
+                    {{ $type->name }}
+                </option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('type_id')" class="mt-2" />
+        </div>
+
+        <div class="mt-6">
+            <x-primary-button>{{ __('Salvar') }}</x-primary-button>
+        </div>
+    </form>
+</x-app-layout>
